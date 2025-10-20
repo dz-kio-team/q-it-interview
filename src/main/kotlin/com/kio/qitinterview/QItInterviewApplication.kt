dddd48b98eaeb4c3e18b44f5ede5385ba6bd538a
chanbinme <gksmfcksqls@gmail.com>
2025-10-20 22:32:08 +0900

    chore: q-it-interview 프로젝트 생성

---
+++ src/main/kotlin/com/kio/qitinterview/QItInterviewApplication.kt
@@ -0,0 +1,11 @@
+package com.kio.qitinterview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QItInterviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<QItInterviewApplication>(*args)
+}
---
+++ src/test/kotlin/com/kio/qitinterview/QItInterviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kio.qitinterview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QItInterviewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
