1b652eb7ea601a316cd540823e8697f6a696a60f
chanbinme <gksmfcksqls@gmail.com>
2025-11-30 23:37:29 +0900

    chore: llm-client 라이브러리 의존성 추가

--- src/main/kotlin/com/kio/qitinterview/QItInterviewApplication.kt
+++ src/main/kotlin/com/kio/qitinterview/QItInterviewApplication.kt
@@ -1,9 +1,11 @@
 package com.kio.qitinterview
 
 import com.kio.qit.annotation.EnableCoreLibrary
+import com.kio.qitllmclient.annotation.EnableLlmLibrary
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@EnableLlmLibrary  // LLM 라이브러리 스프링 빈 등록
 @EnableCoreLibrary  // core library 스프링 빈 등록
 @SpringBootApplication
 class QItInterviewApplication
