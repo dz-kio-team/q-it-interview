1fc84f9840d40eed07f12b6293a61e0a9f644a9b
chanbinme <gksmfcksqls@gmail.com>
2025-12-02 00:19:55 +0900

    feat: JPA Auditing 활성화

--- src/main/kotlin/com/kio/qitinterview/QItInterviewApplication.kt
+++ src/main/kotlin/com/kio/qitinterview/QItInterviewApplication.kt
@@ -4,7 +4,9 @@ import com.kio.qit.annotation.EnableCoreLibrary
 import com.kio.qitllmclient.annotation.EnableLlmLibrary
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableJpaAuditing  // JPA Auditing 활성화
 @EnableLlmLibrary  // LLM 라이브러리 스프링 빈 등록
 @EnableCoreLibrary  // core library 스프링 빈 등록
 @SpringBootApplication
