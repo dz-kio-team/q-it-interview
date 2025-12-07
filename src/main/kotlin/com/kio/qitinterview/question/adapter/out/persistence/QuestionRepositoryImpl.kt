30d4df9172a41f57c46ab00e4838b15aac37ce2c
chanbinme <gksmfcksqls@gmail.com>
2025-12-07 17:33:28 +0900

    feat: Question 조회/생성 로직 추가

--- src/main/kotlin/com/kio/qitinterview/question/adapter/out/persistence/QuestionRepositoryImpl.kt
+++ src/main/kotlin/com/kio/qitinterview/question/adapter/out/persistence/QuestionRepositoryImpl.kt
@@ -4,19 +4,31 @@ import com.kio.qit.annotation.PersistenceAdapter
 import com.kio.qitinterview.question.application.port.out.QuestionRepository
 import com.kio.qitinterview.question.domain.model.AiQuestion
 import com.kio.qitinterview.question.domain.model.CustomQuestion
+import com.kio.qitinterview.question.domain.model.QuestionSuggestionRequest
+import com.kio.qitinterview.question.domain.model.QuestionSuggestionResult
 
 @PersistenceAdapter
 class QuestionRepositoryImpl(
     private val jpaCustomQuestionRepository: JpaCustomQuestionRepository,
-    private val jpaAiQuestionRepository: JpaAiQuestionRepository
+    private val jpaAiQuestionRepository: JpaAiQuestionRepository,
+    private val jpaQuestionSuggestionRequestRepository: JpaQuestionSuggestionRequestRepository,
+    private val jpaQuestionSuggestionResultRepository: JpaQuestionSuggestionResultRepository
 ) : QuestionRepository {
 
-    override fun save(aiQuestion: AiQuestion) : AiQuestion {
-        return jpaAiQuestionRepository.save(aiQuestion)
+    override fun saveAllAiQuestions(aiQuestions: List<AiQuestion>) : List<AiQuestion> {
+        return jpaAiQuestionRepository.saveAll(aiQuestions)
     }
 
-    override fun save(customQuestion: CustomQuestion): CustomQuestion {
-        return jpaCustomQuestionRepository.save(customQuestion)
+    override fun saveAllCustomQuestions(customQuestions: List<CustomQuestion>): List<CustomQuestion> {
+        return jpaCustomQuestionRepository.saveAll(customQuestions)
+    }
+
+    override fun saveAllSuggestionResults(suggestionResults: List<QuestionSuggestionResult>): List<QuestionSuggestionResult> {
+        return jpaQuestionSuggestionResultRepository.saveAll(suggestionResults)
+    }
+
+    override fun saveSuggestionRequest(suggestionRequest: QuestionSuggestionRequest): QuestionSuggestionRequest {
+        return jpaQuestionSuggestionRequestRepository.save(suggestionRequest)
     }
 
 }
--- src/main/kotlin/com/kio/qitinterview/question/application/port/out/QuestionRepository.kt
+++ src/main/kotlin/com/kio/qitinterview/question/application/port/out/QuestionRepository.kt
@@ -2,8 +2,12 @@ package com.kio.qitinterview.question.application.port.out
 
 import com.kio.qitinterview.question.domain.model.AiQuestion
 import com.kio.qitinterview.question.domain.model.CustomQuestion
+import com.kio.qitinterview.question.domain.model.QuestionSuggestionRequest
+import com.kio.qitinterview.question.domain.model.QuestionSuggestionResult
 
 interface QuestionRepository {
-    fun save(aiQuestion: AiQuestion) : AiQuestion
-    fun save(customQuestion: CustomQuestion) : CustomQuestion
+    fun saveAllAiQuestions(aiQuestions: List<AiQuestion>) : List<AiQuestion>
+    fun saveAllCustomQuestions(customQuestions: List<CustomQuestion>) : List<CustomQuestion>
+    fun saveAllSuggestionResults(suggestionResults: List<QuestionSuggestionResult>) : List<QuestionSuggestionResult>
+    fun saveSuggestionRequest(suggestionRequest: QuestionSuggestionRequest) : QuestionSuggestionRequest
 }
